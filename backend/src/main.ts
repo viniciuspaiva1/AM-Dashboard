34c3ce7e08f968f4f1c80232fa6933ba971a789f
Vinicius <viniciuspaivarpg@gmail.com>
2026-01-31 14:35:38 -0300

    chore: habilita o cors para receber req local

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "http://localhost:5173", // A URL do seu projeto Vite
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Permite o envio de cookies/headers de autenticação
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
